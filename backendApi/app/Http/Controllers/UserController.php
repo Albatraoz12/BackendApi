290a4adbd06e9d3c8748f145b5efc2a1ec819523
Dimosthenis Emmanouil <dimosthenis.emmanouil@chasacademy.se>
2022-03-29 16:27:48 +0200

    fix userController

--- backendApi/app/Http/Controllers/UserController.php
+++ backendApi/app/Http/Controllers/UserController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Http\Requests\UserRegisterRequest;
 use App\Models\User;
 use Illuminate\Http\Request;
+use Validator;
 use Illuminate\Support\Facades\Hash;
 
 class UserController extends Controller
